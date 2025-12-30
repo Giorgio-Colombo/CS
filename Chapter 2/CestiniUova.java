4cf819101b141023e7cd40f4cbf4f87899c053a4
Giorgio Colombo <82573473+Giorgio-Colombo@users.noreply.github.com>
2025-12-30 22:26:28 +0000

    Fix: Chiuso scanner con Try

--- Chapter 2/CestiniUova.java
+++ Chapter 2/CestiniUova.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
 
 public class CestiniUova {
+  
     public static void main(String arg[]){
         int numeroDiCestini, uovaPerCestino, totaleUova;
 
         //numeroDiCestini = 10;
         //uovaPerCestino = 6;
         
-        Scanner tastiera = new Scanner(System.in);
-        System.out.println("Inserisci il numero di uova per ciascun cestino: ");
-        uovaPerCestino = tastiera.nextInt();
-        System.out.println("Inserisci il numero di cestini: ");
-        numeroDiCestini = tastiera.nextInt();
-        totaleUova  = numeroDiCestini * uovaPerCestino;
-        
-        System.out.println("Se hai");
-        System.out.println(uovaPerCestino + " uova per cestino e");
-        System.out.println(numeroDiCestini + " cestini");
-        System.out.println("il numero totale di uova è " + totaleUova);
+        try (Scanner tastiera = new Scanner(System.in)) {
+            System.out.println("Inserisci il numero di uova per ciascun cestino: ");
+            uovaPerCestino = tastiera.nextInt();
+            System.out.println("Inserisci il numero di cestini: ");
+            numeroDiCestini = tastiera.nextInt();
+            totaleUova  = numeroDiCestini * uovaPerCestino;
+            
+            System.out.println("Se hai");
+            System.out.println(uovaPerCestino + " uova per cestino e");
+            System.out.println(numeroDiCestini + " cestini");
+            System.out.println("il numero totale di uova è " + totaleUova);
+        }
     }
 }
